2388045ff90838fc8f2727fdeb563be36e7898bd
adammhancock@gmail.com <adammhancock@gmail.com>
2020-08-06 10:17:31 +0100

    fixed entrypoint script

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RUN mkdir -p /var/lib/ghost/content/adapters/storage
-RUN cp -vR node_modules/ghost-azure-storage /var/lib/ghost/content/adapters/storage/
-RUN chown -R node:node /var/lib/ghost/content/adapters/content
+mkdir -p /var/lib/ghost/content/adapters/storage
+cp -vR node_modules/ghost-azure-storage /var/lib/ghost/content/adapters/storage/
+chown -R node:node /var/lib/ghost/content/adapters/content
 exec docker-entrypoint.sh "$@"
