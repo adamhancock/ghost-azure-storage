89443451ccf884316c004543fddd5d1bb934dd5f
adammhancock@gmail.com <adammhancock@gmail.com>
2020-08-06 11:22:21 +0100

    new build

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p content/adapters/storage
-cp -vR node_modules/ghost-azure-storage content/adapters/storage/
+cp -vR node_modules/ghost-azure-storage content/adapters/
 chown -R node:node ./content
 exec docker-entrypoint.sh "$@"
